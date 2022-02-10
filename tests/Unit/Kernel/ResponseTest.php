854c473d91fa898f5945ab08e77a6209c0d02ba9
Harsh Bhatia <harshbhatia@live.co.uk>
2022-02-10 21:48:57 +0000

    refactor scheduler test

--- tests/Unit/Kernel/ResponseTest.php
+++ tests/Unit/Kernel/ResponseTest.php
@@ -10,13 +10,14 @@ class KernelTest extends TestCase
 {
 
     /** @test */
-    public function testIsAvailableInTheScheduler()
+    public function check_if_scheduler_is_running_the_get_booking_command_every_30_mins(): void
     {
         $schedule = app()->make(Schedule::class);
 
-        $events = collect($schedule->events())->filter(function (Event $event) {
-            return stripos($event->command, 'get:booking');
-        });
+        $events = collect($schedule->events())
+            ->filter(function (Event $event) {
+                return stripos($event->command, 'get:booking');
+            });
 
         if ($events->count() == 0) {
             $this->fail('No events found');
